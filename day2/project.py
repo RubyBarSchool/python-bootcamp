af74c8fe4d5c796f29481e97ae1377d663b8f504
truongtv <truongtv1399it@gmail.com>
2025-07-27 09:59:55 +0700

    Update code: Fix this division by zero (sonar)

--- day2/project.py
+++ day2/project.py
@@ -2,5 +2,5 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15: $"))
 people = int(input("How many people to split the bill? "))
-
+if people == 0 : people = 1
 print("Each person should pay: $" + str(format(round((bill+bill*tip/100)/people,2),".2f")))
